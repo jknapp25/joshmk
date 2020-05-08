e80e9c44861943a8e9b27df89f5f721b8bae177c
Josh <joshknappp@gmail.com>
2020-05-07 18:35:32 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8736148eb6ada02540ca25b77cb3fc40.js"
+  "/precache-manifest.80a291883360ba883e1155cf1ce9bec3.js"
 );
 
 self.addEventListener('message', (event) => {
