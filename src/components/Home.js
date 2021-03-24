f03e3676489f1d718cf9ff7abbcdd7574206e015
Josh <joshknappp@gmail.com>
2021-03-23 22:27:31 -0700

    Add hamburger menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -84,7 +84,7 @@ function Home({ children }) {
           avatarUrl={avatarUrl}
           children={children}
         />
-        <div className="my-5" />
+        <div className="my-5 hidden-above-mlg hidden-md hidden-xs" />
         {children}
       </>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { navigate, useLocation } from "@reach/router";
 import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { slide as Menu } from "react-burger-menu";
+import { FiMenu, FiX } from "react-icons/fi";
 export default NavBar;
 
 function NavBar({ config, bgClass = "bg-white" }) {
   const { pathname } = useLocation();
+  const [sideMenuOpen, setSideMenuOpen] = useState(false);
 
   if (!config.pagesCustom || config.pagesCustom.length === 0) return null;
   return (
-    <Container fluid className={`pt-0 ${bgClass} dashboard-container`}>
-      <Row>
-        <Col className="px-0">
-          <Navbar>
-            <Nav className="mr-auto">
-              <h2 className="mb-0">
-                <Nav.Link
-                  href="#"
-                  active={pathname === "/"}
-                  onClick={() => navigate("/")}
-                >
-                  {config.fullName || ""}
-                </Nav.Link>
-              </h2>
-            </Nav>
-            <Nav>
-              {config.pagesCustom.map((page, i) => (
-                <h4
-                  key={page.name}
-                  className={`mb-0 ${
-                    i !== config.pagesCustom.length - 1 ? "mr-3" : ""
-                  }`}
-                >
-                  <Nav.Link
-                    key={page.name}
-                    href="#"
-                    active={pathname === `/${page.link}`}
-                    onClick={() => navigate(`/${page.link}`)}
-                  >
-                    {page.name}
-                  </Nav.Link>
-                </h4>
-              ))}
-            </Nav>
-          </Navbar>
+    <>
+      <Menu
+        right
+        noOverlay
+        disableAutoFocus
+        width={280}
+        isOpen={sideMenuOpen}
+        className="bg-dark text-light"
+      >
+        <Col>
+          <Row className="mt-3">
+            <Col>
+              <FiX
+                size="2em"
+                className="cursor-pointer float-right mr-2"
+                onClick={() => setSideMenuOpen(false)}
+              />
+            </Col>
+          </Row>
+          {config.pagesCustom.map((page, i) => (
+            <Row
+              onClick={() => {
+                setSideMenuOpen(false);
+                navigate(`/${page.link}`);
+              }}
+            >
+              <Col>
+                <h5 className="cursor-pointer">{page.name}</h5>
+              </Col>
+            </Row>
+          ))}
         </Col>
-      </Row>
-    </Container>
+      </Menu>
+      <Container fluid className={`pt-0 ${bgClass} dashboard-container`}>
+        <Row>
+          <Col className="px-0">
+            <Navbar>
+              <Nav className="mr-auto">
+                <h2 className="mb-0">
+                  <Nav.Link
+                    href="#"
+                    active={pathname === "/"}
+                    onClick={() => navigate("/")}
+                  >
+                    {config.fullName || ""}
+                  </Nav.Link>
+                </h2>
+              </Nav>
+              <Nav className="hidden-mlg">
+                {config.pagesCustom.map((page, i) => (
+                  <h4
+                    key={page.name}
+                    className={`mb-0 ${
+                      i !== config.pagesCustom.length - 1 ? "mr-3" : ""
+                    }`}
+                  >
+                    <Nav.Link
+                      key={page.name}
+                      href="#"
+                      active={pathname === `/${page.link}`}
+                      onClick={() => navigate(`/${page.link}`)}
+                    >
+                      {page.name}
+                    </Nav.Link>
+                  </h4>
+                ))}
+              </Nav>
+              <Nav className="hidden-above-mlg">
+                <h2 className="mb-0">
+                  <Nav.Link
+                    href="#"
+                    onClick={() => setSideMenuOpen(true)}
+                    className="pt-0"
+                  >
+                    <FiMenu className="cursor-pointer" />
+                  </Nav.Link>
+                </h2>
+              </Nav>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
