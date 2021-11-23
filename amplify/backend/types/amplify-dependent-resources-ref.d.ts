a69711873f9ab23d9489d29f9b714ab44ca94da4
Josh <joshknappp@gmail.com>
2021-11-22 22:07:08 -0800

    Amplify updates:

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "storage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "joshmk": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "joshmka1fd536a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
