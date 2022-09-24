e537c4bb3b21ad256020c3f6cbd12dac80d9da05
Josh Knapp <joshknappp@gmail.com>
2022-09-24 00:04:03 +0000

    Try tag fix

--- src/components/TagEditor.js
+++ src/components/TagEditor.js
@@ -6,11 +6,11 @@ export default TagEditor;
 
 function TagEditor({ tags = [], tagsOptions = [], onChange }) {
   const selectTags =
-    tags?.length > 0
+    tags && tags.length > 0
       ? tags.map((tag) => ({ label: tag, value: tag }))
       : null;
   const selectTagsOptions =
-    tagsOptions?.length > 0
+    tagsOptions && tagsOptions.length > 0
       ? tagsOptions.map((tag) => ({ label: tag, value: tag }))
       : null;
 
