1b7fd9849537b4d0c413263ae16e03a12a55d2e6
Josh Knapp <joshknappp@gmail.com>
2022-12-30 12:14:01 -0800

    Fix item page styling

--- src/views/Item.js
+++ src/views/Item.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router-dom";
 import useIsMounted from "../lib/useIsMounted";
 import { API } from "aws-amplify";
@@ -8,7 +7,7 @@ import * as queries from "../graphql/queries";
 import ImageCarousel from "../components/ImageCarousel";
 import RichTextEditor from "../components/RichTextEditor/RichTextEditor";
 import ItemBuyButton from "../components/ItemBuyButton";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Heading, VStack } from "@chakra-ui/react";
 
 export default Item;
 
@@ -42,55 +41,36 @@ function Item({ item = {} }) {
   description = description ? JSON.parse(description) : description;
 
   return (
-    <Row className="gx-5 d-flex justify-content-center">
-      <Col md="10">
-        <Row className="mb-5">
-          <Col className="d-flex justify-content-center">
-            <ImageCarousel images={images} isItem />
-          </Col>
-        </Row>
-        <Row className="mb-2 d-flex justify-content-center">
-          <Col md="6">
-            <h1 className="mb-4 text-center">{name}</h1>
+    <VStack spacing={10}>
+      <ImageCarousel images={images} isItem />
 
-            <ItemBuyButton
-              isForSale={isForSale}
-              isSold={isSold}
-              price={price}
-              classes="mt-2 text-center mb-5"
-            />
+      <VStack spacing={5}>
+        <Heading size="md">{name}</Heading>
+        <ItemBuyButton isForSale={isForSale} isSold={isSold} price={price} />
+      </VStack>
 
-            {description ? (
-              <>
-                <h4>Description</h4>
-                <RichTextEditor
-                  value={description}
-                  onChange={() => {}}
-                  readOnly={true}
-                />
-              </>
-            ) : null}
+      {description ? (
+        <RichTextEditor
+          value={description}
+          onChange={() => {}}
+          readOnly={true}
+        />
+      ) : null}
 
-            {tags?.length > 0 ? (
-              <>
-                <h4 className="mt-4">Tags</h4>
-                <div className="border-0 py-0">
-                  {tags.map((tag) => (
-                    <Button
-                      key={`tag-${tag}`}
-                      size="xs"
-                      colorScheme="gray"
-                      onClick={() => navigate(`/search?tag=${tag}`)}
-                    >
-                      {tag}
-                    </Button>
-                  ))}
-                </div>
-              </>
-            ) : null}
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+      {tags?.length > 0 ? (
+        <Box>
+          {tags.map((tag) => (
+            <Button
+              key={`tag-${tag}`}
+              size="xs"
+              colorScheme="gray"
+              onClick={() => navigate(`/search?tag=${tag}`)}
+            >
+              {tag}
+            </Button>
+          ))}
+        </Box>
+      ) : null}
+    </VStack>
   );
 }
